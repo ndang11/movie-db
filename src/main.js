f78122e5497882b9fc746d89f811a9f27baef65b
ndang.royalty <royaltyhopsin@gmail.com>
2024-12-16 10:41:10 +0100

    final torches

--- src/main.js
+++ src/main.js
@@ -28,6 +28,7 @@ document.querySelector("#app").innerHTML = `
       </div>
     </div>
       <div class="main">
+      <div class="main1">
         <button id="btn1">Season3</button>
         <h2>The Mandolorian</h2>
         <small id="time">2h40m.2022.Fantasy.Actions</small>
@@ -37,11 +38,12 @@ document.querySelector("#app").innerHTML = `
              on their journey by the fellow Mandolorian Bo-Katan Kryze.
             </p>
          <br><br>
-      <div class="button">  
+      <div class="button1">  
         <button id="btn2"><img width="20" height="20" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/circled-play.png" alt="circled-play"/>Play Now</button>
         <button id="btn3"><img width="20" height="20" src="https://img.icons8.com/material-outlined/24/circled-play.png" alt="circled-play"/>Watch Trailer</button>
         <button id="btn4"><img width="20" height="20" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/bookmark-ribbon.png" alt="bookmark-ribbon"/>Add Watchlist</button>
       </div>  
+      </div>
       </div>
     </div>
      <br><br>
@@ -59,7 +61,7 @@ document.querySelector("#app").innerHTML = `
         
       </div>
       </section>
-      <section>
+      <>
     <div class="pop swiper">
       <h3>Popular of the week</h3>
       <hr><hr>
@@ -86,7 +88,7 @@ document.querySelector("#app").innerHTML = `
         </div>
         <div class="week3  swiper-slide ">
           <h1>3</h1>
-          <imgsrc="/images/pic33.jpg" alt="pic33"/>
+          <img src="/images/pic33.jpg" alt="pic33"/>
           <div class="sub-three">
           <button id="btn7">PG-13</button>
           <h4>The Flash</h4>
@@ -123,132 +125,24 @@ document.querySelector("#app").innerHTML = `
     </div>
     </section>
     <section>
-      <div class="release">
-        <h3>Just Release</h3>
-        <div class="release1">
-          <img src="" id="photo1" alt="photo"/>
-          <h3 id="dungeon"></h3>
-          <small><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.6 | Mystery.Movie</small>
-        </div>
-        <div class="release2">
-          <img src="" id="photo2" alt="photo"/>
-          <h3 id="Barie"></h3>
-          <small><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.6 | Season 1.Action.Drama</small>
-        </div>
-        <div class="release3">
-          <img src="" id="photo3" alt="photo"/>
-          <h3 id="super"></h3>
-          <small><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.6 | Action.Movie</small>
-        </div>
-        <div class="release4">
-          <img src="" id="photo4" alt="photo"/>
-          <h3 id="slave"></h3>
-          <small><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.6 | Horror.Movie</small>
-        </div>
-        <div class="release5">
-          <img src="" id="photo5" alt="photo"/>
-          <h3 id="enola"></h3>
-          <small><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.8 | Action.Movie</small>
+      <div class="galary-sub">
+        <div class="galaxy">
+          <h3>Explore by genre</h3>
+          <h1>Guardian of The Galaxy; Volume 3</h1>
+          <p><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.6 | 2h40m.2022.Superhero.Actoin</p>
+          <br>
+          <button id="btn8"><img width="24" height="24" src="https://img.icons8.com/material-outlined/24/play-button-circled--v1.png" alt="play-button-circled--v1"/>Play Now</button>
+          <button id="btn9"><img width="24" height="24" src="https://img.icons8.com/ios-glyphs/30/000000/bookmark-ribbon.png" alt="bookmark-ribbon"/>Add Watchlist</button>
         </div>
       </div>
     </section>
-    <section>
-      <div class="watchlist">
-        <h2>Your Watchlist</h2>
-        <div class="watch1">
-          <img src="" id="pic1" alt="pic"/>
-          <h3 id="top"></h3>
-          <small><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.6 | Action.Movie</small>
-        </div>
-        <div class="watch2">
-          <img src="" id="pic2" alt="pic"/>
-          <h3 id="spiderman"></h3>
-          <small><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.6 | Action.Movie</small>
-        </div>
-        <div class="watch3">
-        <img src="" id="pic3" alt="pic"/>
-        <h3 id="black"></h3>
-          <small><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.6 | Action.Movie</small>
-        </div>
-        <div class="watch4">
-          <img src="" id="pic4" alt="pic"/>
-          <h3 id="batman"></h3>
-          <small><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.6 | Action.Movie</small>
-        </div>
-        <div class="watch5">
-          <img src="" id="pic5" alt="pic"/>
-          <h3 id="red"></h3>
-          <small><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.6 | Action.Movie</small>
-        </div>
-    </section>
-    <section>
-      <div class="like">
-        <h2>Your Likes</h2>
-        <div class="like1">
-          <img src="" id="post1" alt="pos"/>
-          <h3 id="thor"></h3>
-          <small><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.6 | Action.Movie</small>
-        </div>
-        <div class="like2">
-          <img src="" id="post2" alt="pos"/>
-          <h3 id="green"></h3>
-          <small><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.6 | Action.Movie</small>
-        </div>
-        <div class="like3">
-          <img src="" id="post3" alt="pos"/>
-          <h3 id="mechanic"></h3>
-          <small><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.6 | Action.Movie</small>
-        </div>
-        <div class="like4">
-          <img src="" id="post4" alt="pos"/>
-          <h3 id="pirates"></h3>
-          <small><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.6 | Action.Movie</small>
-        </div>
-        <div class="like5">
-          <img src="" id="post5" alt="pos"/>
-          <h3 id="spider3"></h3>
-          <small><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.6 | Action.Movie</small>
-        </div>
-      </div>
-    </section>
-    <section>
-      <div class="galaxy">
-        <h3>Explore by genre</h3>
-        <h1>Guardian of The Galaxy; Volume 3</h1>
-        <small><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png" alt="star--v1"/>4.6 | 2h40m.2022.Superhero.Actoin</small>
-        <button id="btn8"><img width="24" height="24" src="https://img.icons8.com/material-outlined/24/play-button-circled--v1.png" alt="play-button-circled--v1"/>Play Now</button>
-        <button id="btn9"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 24 24">
-          <path d="M 5.5 2 A 0.50005 0.50005 0 0 0 5 2.5 L 5 21.5 A 0.50005 0.50005 0 0 0 5.7089844 21.953125 L 12 19.050781 L 18.291016 21.953125 A 0.50005 0.50005 0 0 0 19 21.5 L 19 2.5 A 0.50005 0.50005 0 0 0 18.5 2 L 5.5 2 z M 6 3 L 18 3 L 18 20.71875 L 12.208984 18.046875 A 0.50005 0.50005 0 0 0 11.791016 18.046875 L 6 20.71875 L 6 3 z"></path>
-          </svg>Add Watchlist</button>
-      </div>
-      <div class="arrow">
-        <span><img width="26" height="26" src="https://img.icons8.com/metro/26/FAB005/back.png" alt="back"/></span>
-        <span><img width="26" height="26" src="https://img.icons8.com/metro/26/FAB005/forward.png" alt="forward"/></span>
-        <span><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/FAB005/ellipsis.png" alt="ellipsis"/></span>
-      </div>
-    </section>
-    <article>
-      <div class="movie-img">
-        <img src="" alt="picc"/>
-        <h3>Superhero</h3>
-        <img src="" alt="picc"/>
-        <h3>Drama</h3>
-        <img src="" alt="picc"/>
-        <h3>Sitcom</h3>
-        <img src="" alt="picc"/>
-        <h3>Thriller</h3>
-        <img src="" alt="picc"/>
-        <h3>Comedy</h3>
-        <img src="" alt="picc"/>
-        <h3>Fantasy</h3>
-      </div>
-    </article>
+    <br><br>
     <footer>
       <div class="clossing">
         <div class="plateform">
           <h1>Our platform is trusted<br>by millions & features<br>best updated movies<br>all around the world.</h1>
           <br><br>
-          <small>Privacy policy| Term of service| Language</small>
+          <small>Privacy policy | Term of service | Language</small>
         </div>
         <div class="media">
           <ul>
@@ -264,7 +158,7 @@ document.querySelector("#app").innerHTML = `
             <img width="50" height="50" src="https://img.icons8.com/external-basicons-solid-edtgraphics/50/FFFFFF/external-Tweet-chat-bubbles-basicons-solid-edtgraphics.png" alt="external-Tweet-chat-bubbles-basicons-solid-edtgraphics"/>
             <img width="50" height="50" src="https://img.icons8.com/glyph-neue/64/FFFFFF/google-logo.png" alt="google-logo"/>
           </div> 
-          <small>2023</small>      
+          <small><img width="20" height="20" src="https://img.icons8.com/ios-filled/50/FFFFFF/copy-ai.png" alt="copy-ai"/>2023</small>      
         </div>
       </div>
     </footer>
