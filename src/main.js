ae00b583192cc8a9e30f3efcfce8a987f5c9ddbc
ndang.royalty <royaltyhopsin@gmail.com>
2025-01-02 23:18:18 +0100

    download page

--- src/main.js
+++ src/main.js
@@ -249,3 +249,30 @@ fetch(
   .catch((err) => console.error(err));
   
 
+
+
+  fetch(
+    "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1", options
+  )
+    .then((res) => res.json())
+    .then(({ results }) => {
+      console.log(results);
+      const topRatedMovies = document.getElementById("top-rated");
+  
+      for (let index = 0; index < results.length; index++) {
+        topRatedMovies.innerHTML += `
+        <div class="img-container">
+              <h2>${results[index].original_title} </h2>
+        <img src="https://image.tmdb.org/t/p/w500${results[index].poster_path}" alt="${results[index].original_title}" />
+        <p><span id="star">&#11088;</span>${results[index].vote_count}   <span id="thumbs">&#128077</span></p>
+        </div>
+  
+        `;
+        // const img =document.createElement("img");
+        // img.className = "image-poster";
+  
+        // topRatedMovies.appendChild(img);
+      }
+    })
+    .catch((err) => console.error(err));
+  
